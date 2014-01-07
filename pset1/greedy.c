da8df50c9d4c0acd6f925666d46e4bcbc05772ba
Juan Pablo Rinaldi <juampi@juampi.com>
2014-01-07 07:58:55 -0500

    Add source files for Problem Set 1

---
+++ pset1/greedy.c
@@ -0,0 +1,33 @@
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int coin_count = 0;
+    float dollars;
+    
+    do {
+        printf("Show me the money: ");
+        dollars = GetFloat();
+    } while (dollars < 0);
+    
+    int cents = (int) round(dollars*100);
+    
+    // Count quarters
+    coin_count += cents/25;
+    cents %= 25;
+    
+    // Count dimes
+    coin_count += cents/10;
+    cents %= 10;
+    
+    // Count nickles
+    coin_count += cents/5;
+    cents %= 5;
+    
+    // Count cents
+    coin_count += cents;
+    
+    printf("%d\n", coin_count);
+}
---
+++ pset1/mario.c
@@ -0,0 +1,39 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void drawFloor(int floor_width, int pyramid_height);
+void drawPyramid(int pyramid_height);
+
+int main(void)
+{
+    int pyramid_height;
+    
+    do {
+        printf("Pyramid height: ");
+        pyramid_height = GetInt();
+    } while (pyramid_height < 0 || pyramid_height > 23);
+    
+    drawPyramid(pyramid_height);    
+}
+
+void drawFloor(int floor_width, int pyramid_height)
+{
+    int i;
+    
+    // Indent floor
+    for (i = 0; i <= pyramid_height-floor_width; i++)
+        printf(" ");
+        
+    // Draw floor
+    for (i = 0; i < floor_width; i++)
+        printf("#");
+        
+    printf("\n");
+}
+
+void drawPyramid(int pyramid_height)
+{
+    // Here we use the fact that the bottom floor width is equal to the pyramid height plus 1
+    for(int floor_width = 2; floor_width <= pyramid_height+1; floor_width++)
+        drawFloor(floor_width, pyramid_height);
+}
